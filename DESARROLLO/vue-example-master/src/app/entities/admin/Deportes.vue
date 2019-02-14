67de3c2a8abe705f21d77d20df8074dabf2d0405
Sandra Dopico Cantarero <sandra.dopico1@udc.es>
2019-02-14 13:22:21 +0100

    Interfaz deportes

--- DESARROLLO/vue-example-master/src/app/entities/admin/Deportes.vue
+++ DESARROLLO/vue-example-master/src/app/entities/admin/Deportes.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div>
    <div class="float-right">
       <b-btn        class="button"
@@ -6,16 +7,33 @@
                     @click="back()"><span>Atrás</span></b-btn>
   </div>
 
-<div class= "sport" v-for="sport in sports" :key="sport.idSport">
-    <div class= "tittle">
-    {{ sport.type}}
-      </div>
 
- 
+ <div class="container" ng-controller="index">
 
+      <div class="box margin-right" id="shopping-list">
+        <h1>Deportes</h1>
+        
+   <div class="half margin-right">
+         <input type='text'   class="searchButton" placeholder='Nombre del deporte' ng-model="itemName" autofocus required>
+        </div>
+        
+        
+        <button id="addItem">Nuevo Deporte</button>
+        <div class="list">
+          <table >
+            <tr class="table-header">
+              <td></td>
+            </tr>
+            <tr >
+              <div class= "sport" v-for="sport in sports" :key="sport.idSport">
+                <td>{{ sport.type}}</td>
+             </div>             
+            </tr>
+          </table>  
+    </div>
 </div>
 </div>
-
+</div>
 </template>
 
 <script>
@@ -89,7 +107,6 @@ export default {
   transition: all 0.5s;
   cursor: pointer;
   margin: 5px;
-
   margin-right:87px;
 
 }
@@ -118,6 +135,84 @@ export default {
   opacity: 1;
   right: 0;
 }
+
+
+$red: #fb887c;
+$green: #AFC7B9;
+$navy: #34495e;
+
+
+.container {
+  width: 500px;
+  height: 100%;
+  font-family: 'Lato', sans-serif;
+  margin:0;
+  margin-left:20px;
+}
+
+.box {
+  width: 70%;
+  background: #f3f3f3;
+  padding: 1em;
+  border-radius: 3px;
+  margin-top:50px;
+
+
+  
+}
+
+h1 {
+  color: #3a3a3a;
+  text-align: center;
+  margin: 1em 0;
+}
+
+table {
+  width: 100%;
+
+  td {
+    padding: 0.75em 0.5em;
+  }
+  
+  tr:nth-child(2n+2){
+    background: darken(#eee, 1%); 
+  }
+}
+
+.searchButton {
+  width: 100%;
+  border: 2px solid #e0e0e0;
+  background: #fff;
+  outline: none;
+  padding: 1em;
+  border-radius: 3px;
+  font-weight: 300;
+  font-size: 1em;
+  margin-top: 0.25em;
+  margin-bottom: 0.25em;
+  position: relative;
+}      
+
+
+
+button {
+  margin: 0.25em 0;
+  display: block;
+  outline: none;
+  background: $red;
+  border: none;
+  padding: 1.1em;
+  border-radius: 6px;
+  width: 100%;
+  color: white;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-bottom: 4px solid darken($red, 5%);
+  
+}
+
+
+
 
 
 
